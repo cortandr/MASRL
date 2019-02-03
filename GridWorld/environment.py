678ebf6bc8e5022f3a89e5675cf7f3132c5b3148
andrea cortoni <cortandr95@gmail.com>
2019-02-03 17:02:43 +0100

    Added even teams generation
    Cluster agetns into two even numbered teams

--- GridWorld/environment.py
+++ GridWorld/environment.py
@@ -21,7 +21,8 @@ class Environment:
 
         self.obstacles = self.generate_random_obstacles(n_rows, n_cols)
         team_agents, team_opponents = self.generate_random_agents(
-                                                    n=n_agents + n_opponents,
+                                                    n_agents=n_agents,
+                                                    n_opponents=n_opponents,
                                                     rows=n_rows,
                                                     cols=n_cols
                                                 )
@@ -56,34 +57,36 @@ class Environment:
         agents_pos = [a.get_position() for a in self.agents]
         opponents_pos = [a.get_position() for a in self.opponents]
 
-        for i in range(self.n_rows):
-            for j in range(self.n_cols):
-                if (i, j) in agents_pos:
-                    grid[i][j] = 1
-                elif (i, j) in opponents_pos:
-                    grid[i][j] = 2
-                elif (i, j) in obstacles_list:
-                    grid[i][j] = -1
-                else:
-                    grid[i][j] = 0
+        for ag_pos in agents_pos:
+            x, y = ag_pos
+            grid[x][y] = 1
 
-        for row in grid:
-            print(row)
+        for opp_pos in opponents_pos:
+            x, y = opp_pos
+            grid[x][y] = 2
+
+        for obs in obstacles_list:
+            x, y = obs
+            grid[x][y] = -1
 
         return grid
 
-    def generate_random_agents(self, n, rows, cols, cluster=True):
+    def generate_random_agents(self, n_agents, n_opponents, rows, cols, cluster=True):
         """
         Generates and places random agents in the environment
-        :param n: number of agents to create
+        :param n_agents: number of agents to create
+        :param n_opponents: number of opponents to create
         :param rows: x dimension of grid
         :param cols: y dimension of grid
         :param cluster: boolean telling whether or not to cluster the to team's position or not
         :return:
         """
 
+        n = n_agents+n_opponents
+
         ag_pos = list()
 
+        # Generate all x, y combinations
         x_axis = [i for i in range(0, rows)]
         y_axis = [j for j in range(0, cols)]
         combos = [(x, y) for x in x_axis for y in y_axis]
@@ -95,29 +98,29 @@ class Environment:
                 del combos[combos.index(item)]
 
         for _ in range(n):
-
+            # Randomly choose agents positions
             pos_choice = random.choice(combos)
-
             ag_pos.append(pos_choice)
-
             del combos[combos.index(pos_choice)]
 
-        team_agents = []
-        team_opponents = []
         if cluster:
-            # TODO: make even sized clusters
+            # Cluster agents in two team
             model = KMeans(n_clusters=2)
             model.fit(ag_pos)
-            labels = model.labels_
-            team_agents = [pos for i, pos in enumerate(ag_pos)
-                           if labels[i] == 0]
-            team_opponents = [pos for i, pos in enumerate(ag_pos)
-                              if labels[i] == 1]
-        # TODO: random teams
+            c1, c2 = model.cluster_centers_
+
+            # Find distances from first center
+            dist_c1 = [{"pos": pos, "dist": point_dist(c1, pos)} for pos in ag_pos]
+
+            # Separate positions into teams
+            team_agents = sorted(dist_c1, key=lambda k: k["dist"], reverse=True)
+            team_agents = [item["pos"] for item in team_agents[:n_agents]]
+        else:
+            team_agents = [pos for pos, _ in zip(ag_pos, range(n_agents))]
+
+        team_opponents = [pos for pos in ag_pos if pos not in team_agents]
 
         return team_agents, team_opponents
-
-
 
 
     @staticmethod
@@ -131,9 +134,11 @@ class Environment:
 
         # TODO: avoid square shaped obstacles
 
+        # Decide number of obstacles and their length
         n_obs = int((rows + cols)//4)
         obs_length = [randint(2, int(rows//2)) for _ in range(n_obs)]
 
+        # Generate all x, y combinations
         x_axis = [i for i in range(0, rows)]
         y_axis = [j for j in range(0, cols)]
         combos = [(x, y) for x in x_axis for y in y_axis]
@@ -142,12 +147,13 @@ class Environment:
 
         for _ in range(n_obs):
             obs = list()
+            # Random obstacle length
             length = random.choice(obs_length)
             del obs_length[obs_length.index(length)]
 
+            # Random position from grid
             choice_pos = random.choice(combos)
             obs.append(choice_pos)
-
             del combos[combos.index(choice_pos)]
 
             for _ in range(length-1):
--- GridWorld/utils.py
+++ GridWorld/utils.py
@@ -1,3 +1,5 @@
+import math
+
 
 def adjacent_cells(adj_x, adj_y, max_x, max_y, curr_obs, combos):
 
@@ -8,3 +10,10 @@ def adjacent_cells(adj_x, adj_y, max_x, max_y, curr_obs, combos):
             if 0 < item[0] < max_x and 0 < item[1] < max_y and
             item not in curr_obs and item in combos]
 
+
+def point_dist(p1, p2):
+
+    x1, y1 = p1
+    x2, y2 = p2
+
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2)
