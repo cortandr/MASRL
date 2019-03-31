Fixed softmax bug

MASGridWorld/Agent/NeuralNet.py
@@ -13,6 +13,7 @@ class Brain:
         self.input_layer = None
         self.target_Q = None
         self.Q_values = None
+        self.Q_dist = None
         self.learning_rate = learning_rate
         self.lr_decay = decay
         self.discount_rate = discount_rate
@@ -23,7 +24,7 @@ class Brain:
         self.saver = None
         self.sess = None
         self.writer = None
-        self.temp = 0.6
+        self.temp = 1.0
         self.training = training
         self.output_dim = output_dim
         self.build_network()
@@ -34,7 +35,7 @@ class Brain:
     def predict(self, input_tensor, allowed_moves):
 
         predictions = self.sess.run(
-            self.Q_values,
+            self.Q_dist,
             feed_dict={
                 self.input_layer: input_tensor,
             })
@@ -153,30 +154,21 @@ class Brain:
                 name="fc_1"
             )(global_avg_pool)
 
-            normalized_fc_1 = tf.keras.layers.BatchNormalization(
-                axis=1,
-                name="BatchNorm_fc_1"
-            )(fc_1)
-
-            dropout_fc_2 = tf.keras.layers.Dropout(
-                rate=0.5,
-                name="Dropout"
-            )(normalized_fc_1)
-
             self.Q_values = tf.keras.layers.Dense(
                 units=self.output_dim,
-                activation="softmax",
+                activation=tf.nn.tanh,
                 name="QValues"
-            )(dropout_fc_2/self.temp)
+            )(fc_1)
+
+            self.Q_dist = tf.nn.softmax(
+                logits=self.Q_values/self.temp,
+                name="Q_Dist"
+            )
 
             # Calculate Loss
             with tf.name_scope("Loss"):
-                # self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q_values))
-                entropy = tf.nn.softmax_cross_entropy_with_logits_v2(
-                    logits=self.Q_values,
-                    labels=self.target_Q
-                )
-                self.loss = tf.reduce_mean(entropy)
+                self.loss = tf.reduce_mean(
+                    tf.square(self.target_Q - self.Q_values))
 
             with tf.name_scope("Global_Step"):
                 self.global_step = tf.Variable(0, trainable=False)

MASGridWorld/environment.py
@@ -1,6 +1,5 @@
 from Agent.agent import Agent, DummyAgent
 from Agent.NeuralNet import Brain
-from random import randint
 from utils import *
 import random
 import copy
@@ -49,9 +48,9 @@ class Environment:
                 a.brain = self.target_net
 
         self.opponents = [DummyAgent(position=pos) for pos in team_opponents]
-        self.r1_w = 0.5
-        self.r2_w = 0.2
-        self.r3_w = 0.3
+        self.r1_w = 0.4
+        self.r2_w = 0.4
+        self.r3_w = 0.2
 
     @property
     def grid(self):
@@ -85,20 +84,20 @@ class Environment:
         # Initialize empty grid
         grid = np.zeros((self.n_rows, self.n_cols, 4), np.int8)
 
-        # Background channel
-        for a in sum(self.obstacles, []):
-            x, y = a
-            grid[x][y][0] += 1.0
+        # Allies channel
+        for a in self.agents:
+            x, y = a.get_position()
+            grid[x][y][1] += 1.0
 
         # Opponents channel
         for a in self.opponents:
             x, y = a.get_position()
-            grid[x][y][1] += 1.0
-
-        # Allies channel
-        for a in self.agents:
-            x, y = a.get_position()
             grid[x][y][2] += 1.0
+
+        # Background channel
+        for a in sum(self.obstacles, []):
+            x, y = a
+            grid[x][y][3] += 1.0
 
         return grid
 
@@ -129,13 +128,13 @@ class Environment:
                 allowed_moves,
                 self.agents)
 
-        state_tensor = np.array([self.brain_input_grid.copy()])
+        state_tensor = np.array([copy.deepcopy(self.brain_input_grid)])
 
         for agent in self.agents:
             allowed_moves = self.allowed_moves(agent)
             temp_state_tensor = copy.deepcopy(state_tensor)
             x, y = agent.get_position()
-            temp_state_tensor[0][x][y][3] = 1
+            temp_state_tensor[0][x][y][0] = 1
             agent.choose_action(allowed_moves, temp_state_tensor)
 
         # Check for overlapping opponents
@@ -151,7 +150,11 @@ class Environment:
         self.opponents = [oppo for oppo in self.opponents
                            if oppo.get_position() not in agents]
 
-        return self.get_reward(int(training_ate))
+        # Get next state
+        next_state = self.brain_input_grid
+        next_state = np.array([copy.deepcopy(next_state)]) if next_state is not None else None
+
+        return state_tensor, next_state, self.get_reward(int(training_ate))
 
     def generate_random_agents(self, n_agents, n_opponents, rows, cols, cluster=True):
         """
@@ -289,28 +292,20 @@ class Environment:
         return len(self.opponents) == 0
 
     def get_reward(self, training_ate):
+
         # Reward 1 -> number of agents
         reward1 = (self.n_agents - self.n_opponents)**2
 
-        range1 = self.n_agents**2
-        # range1 = self.allies ** 2 - (self.allies - self.opponents) ** 2
-        # reward1 = (reward1 - (self.allies - (self.opponents ** 2))) / range1
-        reward1 = reward1 / range1
-
-        # bottom_limit = self.allies - (self.opponents ** 2)
-        # # top limit is self.allies
-        # reward_range = self.allies - bottom_limit
-        # shift = (reward_range / 2) - self.allies
-        # reward1 = (reward1 + shift) / (reward_range / 2)
+        range1 = self.n_agents ** 2
+        shift = (range1 / 2)
+        reward1 = (reward1 - shift) / (range1 / 2)
 
         # Reward 2 -> board coverage
         combined = self.reachability()
         reward2 = sum(sum((combined < 0).astype(int))) - \
                   sum(sum((combined > 0).astype(int)))
-        range2 = (self.n_rows*self.n_cols) - \
-                 (-(self.n_rows*self.n_cols))
-        reward2 = reward2 / (self.n_rows * self.n_cols)
-        reward2 = (reward2 - (-100)) / range2
+        range2 = self.n_rows*self.n_cols
+        reward2 = reward2 / range2
 
         return self.r1_w*reward1 + self.r2_w*reward2 + self.r3_w*training_ate
 

MASGridWorld/simulation.py
@@ -47,8 +47,6 @@ class Sim:
         self.viz = viz
         self.viz_execution = viz_execution
         self.train_saving = train_saving
-        self.r1_w = 0.5
-        self.r2_w = 0.5
 
     def run(self):
         """
@@ -72,47 +70,51 @@ class Sim:
             training_agent = next(
                 filter(lambda ag: ag.training, self.environment.agents))
 
+            episode_reward = []
+
             while sim_moves < self.moves_limit and not self.environment.is_over():
 
                 # Current state
-                curr_state = self.environment.brain_input_grid
+                # curr_state = self.environment.brain_input_grid
 
                 # Apply step in Environment
-                reward = self.environment.step()
+                curr_state, next_state, reward = self.environment.step()
 
                 # Get agent chosen action
                 action = training_agent.get_chosen_action()
 
                 # Get state after chosen action is applied
-                next_state = self.environment.brain_input_grid
+                # next_state = self.environment.brain_input_grid
 
-                self.metrics["reward"].append(reward)
+                episode_reward.append(reward)
 
                 # Store transition in replay table
                 self.experience_replay.append({
-                    "state": np.array([curr_state]),
+                    "state": curr_state,
                     "action": action,
-                    "next_state": np.array([next_state]) if next_state is not None else None,
+                    "next_state": next_state,
                     "reward": reward,
                 })
 
                 sim_moves += 1
+
+            self.metrics["reward"].append(sum(episode_reward)/len(episode_reward))
 
             sim += 1
 
             # if sim < 50000:
             #     self.r1_w += 8e-6
             #     self.r2_w -= 8e-6
-            if sim < 10000:
+            if sim < 100000:
                 # training_agent.brain.exploration_rate -= 5e-5
-                training_agent.brain.temp -= 5e-5
+                training_agent.brain.temp -= 9e-6
 
             # Train every 2 simulations
             if sim % 10 == 0:
                 self.train_ally(sim/2)
 
             # Update training net every 10 simulations
-            if sim % 250 == 0:
+            if sim % 1000 == 0:
                 self.update_target_net()
                 print("-------------------------------")
                 print("Sim checkpoint : {}".format(sim))